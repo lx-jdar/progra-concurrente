2082b5fb284c1e845a509ab2db825ff87ae4e84e
lx-jdar <jdar.soft@gmail.com>
2023-09-12 12:33:33 -0300

    adding code for java

---
+++ tp1/java/App.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+    public static List<String> getArguments(String[] args) {
+
+        if (args.length == 0) {
+            return Arrays.asList("B", "C");
+        } else {
+            switch (args[0]) {
+                case "B":
+                    return Arrays.asList("D", "E");
+                case "C":
+                    return Arrays.asList("F");
+                case "E":
+                    return Arrays.asList("G", "H");
+                default:
+                    return Arrays.asList();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        List<String> arguments = getArguments(args);
+        List<Process> children = new ArrayList<Process>();
+
+        ProcessHandle current = ProcessHandle.current();
+        long pid = current.pid();
+        long ppid = current.parent().get().pid();
+        String process = args.length == 0 ? "A" : args[0];
+        System.out.println(process + "-> PID: " + pid + " PPID: " + ppid);
+
+        try {
+            for (String arg : arguments) {
+                ProcessBuilder pb = new ProcessBuilder("java", "App.java", arg);
+                children.add(pb.inheritIO().start());
+            }
+
+            for (Process child : children) {
+                child.waitFor();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
