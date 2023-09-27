74387df7886f015fe458512758f091f3ec2e324e
lx-jdar <jdar.soft@gmail.com>
2023-09-26 21:27:38 -0300

    Adding go code for tp3

---
+++ tp3/go/tp3-golang.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+)
+
+const (
+	LETRA_A      = 65
+	CANT_THREADS = 2
+)
+
+var password []int
+var mtx sync.Mutex
+
+func convertirAEntero(cad string, start int, end int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	mtx.Lock()
+	for i := start; i < end; i++ {
+		password[i] = int(cad[i]) - LETRA_A + 1
+		// fmt.Printf("Char at %d Index Pos = %c int %d \n", i, cad[i], int(cad[i]))
+	}
+	mtx.Unlock()
+}
+
+func main() {
+	if len(os.Args) < 1 {
+		fmt.Println("Please, enter the parameter to convert!")
+	}
+	cadena := os.Args[1]
+	password = make([]int, len(cadena))
+
+	var wg sync.WaitGroup
+	wg.Add(CANT_THREADS)
+	fmt.Println("Analyzing Parte 1!")
+	go convertirAEntero(strings.ToUpper(cadena), 0, 4, &wg)
+	fmt.Println("Analyzing Parte 2!")
+	go convertirAEntero(strings.ToUpper(cadena), 4, len(cadena), &wg)
+	wg.Wait()
+
+	fmt.Println("\nHello World!")
+	fmt.Println(password)
+}
