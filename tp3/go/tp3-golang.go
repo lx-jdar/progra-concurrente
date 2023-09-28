0830cd8caf36d165df5da225be16e10ece4020d6
lx-jdar <jdar.soft@gmail.com>
2023-09-28 01:28:46 -0300

    correcciones go

--- tp3/go/tp3-golang.go
+++ tp3/go/tp3-golang.go
@@ -15,8 +15,9 @@ const (
 )
 
 type CharPackage struct {
-	offset int
-	chars  string
+	threadID string
+	offset   int
+	chars    string
 }
 
 var password []int
@@ -26,6 +27,7 @@ func convertirAEntero(chnl chan CharPackage, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for data := range chnl {
+		fmt.Printf("Thread %s tratando cad : %s\n", data.threadID, data.chars)
 		for idx := INIT_VALUE; idx < len(data.chars); idx++ {
 			mtx.Lock()
 			password[idx+data.offset] = int(data.chars[idx]) - ASCII_START
@@ -50,25 +52,34 @@ func main() {
 	go convertirAEntero(dataT2, &wg)
 
 	cycles := len(cadena) / CHARS_BY_THREAD
-	if len(cadena)%CHARS_BY_THREAD != INIT_VALUE {
-		cycles++
+	if len(cadena) == CHARS_BY_THREAD {
+		cycles = 0
 	}
 	start, offset := INIT_VALUE, CHARS_BY_THREAD
 	for i := INIT_VALUE; i < cycles; i++ {
 		// la logica es delegar de a 4 caracteres a los threads
 		if i%2 == INIT_VALUE {
-			dataT1 <- CharPackage{start, cadena[start:offset]}
+			dataT1 <- CharPackage{"A", start, cadena[start:offset]}
 		} else {
-			dataT2 <- CharPackage{start, cadena[start:offset]}
+			dataT2 <- CharPackage{"B", start, cadena[start:offset]}
 		}
 		// determinos los sig. 4 chars
 		start += CHARS_BY_THREAD
-		if offset+CHARS_BY_THREAD < len(cadena) {
-			offset += CHARS_BY_THREAD
+		offset += CHARS_BY_THREAD
+
+	}
+
+	if offset > len(cadena) {
+		offset = len(cadena)
+	}
+	for idx := start; idx < len(cadena); idx++ {
+		if idx%2 != INIT_VALUE {
+			dataT1 <- CharPackage{"A", idx, string(cadena[idx])}
 		} else {
-			offset = len(cadena)
+			dataT2 <- CharPackage{"B", idx, string(cadena[idx])}
 		}
 	}
+	//fmt.Println(start, offset, cadena[start:offset])
 
 	close(dataT1)
 	close(dataT2)
