3924959ee413d4a74494371e719a30e895fc974d
EACW-UNLAM <111432181+EACW-UNLAM@users.noreply.github.com>
2023-10-11 20:48:12 -0300

    Update procesos.c

--- tp1/c++/procesos.c
+++ tp1/c++/procesos.c
@@ -5,62 +5,55 @@
 #define NO_INI -1
 #define PROS_CREATED 0
 
-void showPidAndPha(char name);
+void showPidAndPhaSi(pid_t pid,char name);
 int main()
 {
- pid_t pIdB=NO_INI;
- pid_t pIdC=NO_INI;
- pid_t pIdD=NO_INI;
- pid_t pIdE=NO_INI;
- pid_t pIdF=NO_INI;
- pid_t pIdG=NO_INI;
- pid_t pIdH=NO_INI;
- pIdB=fork();
- if( pIdB != PROS_CREATED )
- {
-  showPidAndPha('A');
-  pIdC=fork();
- }
- if( pIdC == PROS_CREATED )
- {
-  showPidAndPha('C');
-  pIdF=fork();
- }
- if( pIdB == PROS_CREATED )
- {
-  showPidAndPha('B');
-  pIdE=fork();
-  if( pIdE != PROS_CREATED )
-    pIdD=fork();
- }
- if( pIdE == PROS_CREATED )
- {
-  showPidAndPha('E');
-  pIdG=fork();
-  if( pIdG != PROS_CREATED )
-    pIdH=fork();
- }
- if( pIdD == PROS_CREATED )
- {
-   showPidAndPha('D');
- }
-  if( pIdF == PROS_CREATED )
-  { 
-   showPidAndPha('F');
-  }
-  if( pIdG == PROS_CREATED )
-  { 
-   showPidAndPha('G');
-  }
-  if( pIdH == PROS_CREATED )
+  pid_t pIdB=NO_INI;
+  pid_t pIdC=NO_INI;
+  pid_t pIdD=NO_INI;
+  pid_t pIdE=NO_INI;
+  pid_t pIdF=NO_INI;
+  pid_t pIdG=NO_INI;
+  pid_t pIdH=NO_INI;
+
+  showPidAndPhaSi(PROS_CREATED,'A');
+  pIdB=fork();
+  if( pIdB != PROS_CREATED )
   {
-   showPidAndPha('H');
+   pIdC=fork();
   }
+  if( pIdC == PROS_CREATED )
+  {
+   showPidAndPhaSi(pIdC,'C');
+   pIdF=fork();
+  }
+  showPidAndPhaSi(pIdB,'B');
+  if( pIdB == PROS_CREATED )
+  {
+   pIdE=fork();
+   if( pIdE != PROS_CREATED )
+     pIdD=fork();
+  }
+
+  showPidAndPhaSi(pIdE,'E');
+
+  if( pIdE == PROS_CREATED )
+  {
+   pIdG=fork();
+   if( pIdG != PROS_CREATED )
+    pIdH=fork();
+  }
+
+  showPidAndPhaSi(pIdD,'D');
+  showPidAndPhaSi(pIdF,'F');
+  showPidAndPhaSi(pIdG,'G');
+  showPidAndPhaSi(pIdH,'H');
   sleep(15);
   return 0;
 }
 
-void showPidAndPha(char name)
+void showPidAndPhaSi(pid_t pid,char name)
 {
-  printf("Soy el proceso %c PID:%d y su Padre es:%d\n",name,getpid(),getppid());
+  if( pid == PROS_CREATED )
+    printf("Soy el proceso %c PID:%d y su Padre es:%d\n",name,getpid(),getppid());
 }
