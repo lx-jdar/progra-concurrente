dd1138e47911231d44f14feb6baa8008c427e933
EACW-UNLAM <111432181+EACW-UNLAM@users.noreply.github.com>
2023-11-22 17:36:08 -0300

    Se agregó musica, win y game_over
    Se agregaron funciones.

--- tp_integrador/Juego_2 -2.py
+++ tp_integrador/Juego_2 -2.py
@@ -10,31 +10,57 @@ DESPLAZAMIENTO_X = 43
 DESPLAZAMIENTO_Y = 38
 MULTIPLICADOR_X  = 92
 MULTIPLICADOR_Y  = 68
+DESPLAZAMIENTO_GAME_OVER_X= 200
+DESPLAZAMIENTO_GAME_OVER_Y= 200
+ANCHO_GAME_OVER= 400
+ALTO_GAME_OVER  = 200
 ANCHO_CABALLO = 70
 ALTO_CABALLO  = 50
 TAM_TABLERO = 8
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
-# img_tablero=pygame.image.load("tp_integrador/Imagenes/tablero2.png")
-# fondo = pygame.transform.scale(img_tablero, (800,600))
-# img_x=pygame.image.load("tp_integrador/Imagenes/X.png")
-# ocupado = pygame.transform.scale(img_x, (ANCHO_CABALLO,ALTO_CABALLO))
+
 img_tablero = None
 fondo = None
 img_x = None
 ocupado = None
 ventana = None
 running = True
+global victoria
+global derrota
+derrota=False
+victoria=False
 
 sem_init = threading.Semaphore(0)	
 threads = []
-VELOCIDAD = 1
+
+def derrota_o_victoria():
+        
+        global victoria
+        global derrota
+        victoria=False
+        derrota  = False
+        while True:
+                if caballo.cant_mov == 64:
+                        derrota = True
+                        ventana.blit(game_over,(DESPLAZAMIENTO_GAME_OVER_X,DESPLAZAMIENTO_GAME_OVER_Y))
+                        return;
+                if not(True in caballo.movimientosPosibles.values()) and False in caballo.movimientosPosibles.values():
+                        ventana.blit(game_over,(DESPLAZAMIENTO_GAME_OVER_X,DESPLAZAMIENTO_GAME_OVER_Y))
+                        movimientosPosibles={} 
+                        derrota = True
+                        return;
+                        
+
+                
+                
 def hilo_actualizar(tablero,fila):
         for columna in range (0,TAM_TABLERO):
            if tablero.casillas[fila][columna] == False:
              ventana.blit(ocupado,(DESPLAZAMIENTO_X+fila*MULTIPLICADOR_X,DESPLAZAMIENTO_Y+columna*MULTIPLICADOR_Y)) 
 
 class Tablero:
+        
         casillas = np.full((TAM_TABLERO, TAM_TABLERO),True)
         def actualizar(self):
                 hilos_actua=[]
@@ -49,11 +75,12 @@ class Tablero:
                 self.casillas = np.full((TAM_TABLERO, TAM_TABLERO),True)
                         
 class Caballo:
-        img_caballo=pygame.image.load("tp_integrador/Imagenes/caballo.jpg")
+        img_caballo=pygame.image.load("Imagenes/caballo.jpg")
         pieza_caba = pygame.transform.scale(img_caballo, (ANCHO_CABALLO, ALTO_CABALLO))
         movimientosPosibles={}
         posX = -1
         posY = -1
+        cant_mov=0;
         movimientos_anteriores=[]
         movimientos_deshechos=[]
         def mover( self,new_posX, new_posY,tablero):
@@ -63,14 +90,15 @@ class Caballo:
                   ventana.blit(self.pieza_caba,(DESPLAZAMIENTO_X+new_posX*MULTIPLICADOR_X,DESPLAZAMIENTO_Y+new_posY*MULTIPLICADOR_Y))
                   tablero.casillas[new_posX][new_posY]=False
                   self.calcularMov(tablero)
+                  self.cant_mov+=1
                   return
                 if '{}{}'.format(new_posX,new_posY) in self.movimientosPosibles and  tablero.casillas[new_posX][new_posY]!=False:
-                        ##self.animacion(new_posX,new_posY,tablero)
                         tablero.casillas[self.posX ][self.posY ]=False
                         self.movimientos_anteriores.append((self.posX,self.posY))
                         self.posX = new_posX
                         self.posY = new_posY
                         tablero.actualizar()
+                        self.cant_mov+=1
                         ventana.blit(self.pieza_caba,(DESPLAZAMIENTO_X+new_posX*MULTIPLICADOR_X,DESPLAZAMIENTO_Y+new_posY*MULTIPLICADOR_Y)) 
                 self.movimientosPosibles={}
                 self.calcularMov(tablero)
@@ -87,34 +115,20 @@ class Caballo:
                         ventana.blit(caballo.pieza_caba,(new_pos_pixelX,new_pos_pixelY))
                         self.movimientosPosibles={}
                         self.calcularMov(tablero)
+                        self.cant_mov-=1;
+                       
         def volver_adelante(self,tablero):
                 if self.movimientos_deshechos:
                    new_posX,new_posY=self.movimientos_deshechos.pop()
                    self.mover(new_posX,new_posY,tablero)
+                   
         
         def restart(self):
                 self.movimientosPosibles={}
                 self.posX = -1
                 self.posY = -1
-##        def animacion(self,new_posX,new_posY,tablero):
-##                pos_pixelX_actual=43+self.posX*92;
-##                pos_pixelY_actual=self.posY*68+38;
-##                new_pos_pixelX=43+new_posX*92;
-##                new_pos_pixelY=new_posY*68+38; 
-##                while (pos_pixelX_actual!=new_pos_pixelX):
-##                        if pos_pixelX_actual>new_pos_pixelX :
-##                          pos_pixelX_actual-=VELOCIDAD
-##                          ventana.blit(caballo.pieza_caba,(pos_pixelX_actual,pos_pixelY_actual))
-##                        if pos_pixelX_actual<new_pos_pixelX :
-##                          pos_pixelX_actual+=VELOCIDAD
-##                          ventana.blit(caballo.pieza_caba,(pos_pixelX_actual,pos_pixelY_actual))
-##                while (pos_pixelY_actual!=new_pos_pixelY):
-##                        if pos_pixelY_actual>new_pos_pixelY :
-##                          pos_pixelY_actual-=VELOCIDAD
-##                          ventana.blit(caballo.pieza_caba,(pos_pixelX_actual,pos_pixelY_actual))
-##                        if pos_pixelY_actual<new_pos_pixelY :
-##                          pos_pixelY_actual+=VELOCIDAD
-##                          ventana.blit(caballo.pieza_caba,(pos_pixelX_actual,pos_pixelY_actual))
+                self.cant_mov=0
+
         def get_key(self,val_x,val_y):
                 return '{}{}'.format(self.posX+val_x, self.posY+val_y)   
         
@@ -143,14 +157,7 @@ class Caballo:
                         self.movimientosPosibles[self.get_key(-1,2)]=tablero.casillas[self.posX-1][self.posY+2]
                   if(self.posX + 1) <= 7:
                         self.movimientosPosibles[self.get_key(1,2)]=tablero.casillas[self.posX+1][self.posY+2]
-                
-                print(self.movimientosPosibles)
-                if True in self.movimientosPosibles.values():
-                 print("El diccionario contiene el valor True")
-                else:
-                 print("El diccionario contiene el valor FALSE- PERDISTE")
 
-     
 class ConfigThread(threading.Thread):
 
     def __init__(self):
@@ -158,14 +165,18 @@ class ConfigThread(threading.Thread):
 
     def run(self):
 
-        global  img_tablero, fondo, img_x, ocupado
-        
-        img_tablero = pygame.image.load("tp_integrador/Imagenes/tablero2.png")
+        global  img_tablero, fondo, img_x, ocupado,img_game_over,game_over,win
+        pygame.mixer.music.load('Musica/musica.mp3')
+        pygame.mixer.music.play(-1)
+        img_tablero = pygame.image.load("Imagenes/tablero2.png")
         fondo = pygame.transform.scale(img_tablero, (WINDOW_WIDTH,WINDOW_HEIGHT))
-        
-        img_x = pygame.image.load("tp_integrador/Imagenes/X.png")
+        img_game_over=pygame.image.load("Imagenes/game_over_2.png")
+        game_over= pygame.transform.scale(img_game_over, (ANCHO_GAME_OVER,ALTO_GAME_OVER))
+        img_win=pygame.image.load("Imagenes/win.png")
+        win= pygame.transform.scale(img_win, (ANCHO_GAME_OVER,ALTO_GAME_OVER))
+        img_x = pygame.image.load("Imagenes/X2.png")
         ocupado = pygame.transform.scale(img_x, (ANCHO_CABALLO,ALTO_CABALLO))
-    
+
                       
 def esperar_fin():
         global running
@@ -184,23 +195,19 @@ def configuracion_inicial():
         thread_config_ini.join()
         ventana.blit(fondo,(0,0))
         pygame.display.flip()
-        
-#ventana          = pygame.display.set_mode((800,600))
 
+thread_derrota_o_victoria=threading.Thread(target=derrota_o_victoria)
 thread_esperar_fin = threading.Thread(target=esperar_fin)
-# pygame.init()
-# pygame.display.set_caption("Caballo de Ajedrez")
-# ventana = pygame.display.set_mode((800,600))
 
 
+caballo= Caballo()
 configuracion_inicial()
 thread_esperar_fin.start()
-
+thread_derrota_o_victoria.start()
 tablero=Tablero()
-caballo= Caballo()
-#print(tablero.casillas)
+
+
 while running:
-        # sem_init.acquire()
         for evento in pygame.event.get():
           if evento.type == pygame.QUIT:
                 sem_init.release()
@@ -210,8 +217,12 @@ while running:
                 print(posY)
                 caballo.mover(int((posX-DESPLAZAMIENTO_X)/MULTIPLICADOR_X),int((posY-DESPLAZAMIENTO_Y)/MULTIPLICADOR_Y),tablero)
           elif evento.type == pygame.KEYDOWN:
+             if evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_z :
                  caballo.volver_atras(tablero)
+                 if derrota == True or victoria == True:
+                    thread_derrota_o_victoria=threading.Thread(target=derrota_o_victoria)
+                    thread_derrota_o_victoria.start()
                 elif evento.key == pygame.K_y:
                  caballo.volver_adelante(tablero)
                 elif evento.key == pygame.K_ESCAPE:
@@ -222,12 +233,15 @@ while running:
                  tablero.restart()
                  caballo.restart()
                  tablero.actualizar()
-          
-          pygame.display.update()
-          
-
-
-#thread_esperar_fin.join()
+        
+        pygame.display.update()
+        if derrota == True:
+             thread_derrota_o_victoria.join()
+        elif victoria == True:
+             thread_derrota_o_victoria.join()
+             
+        
+        
 pygame.quit()
 sys.exit()
 
